fe1bf0c8fb04405b38f39913ea19e10672f76e47
howshekilledit <morganhg@uic.edu>
2022-12-16 15:41:44 -0600

    attempt to include windowWidth intersting all over

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup(){
   let count = 0
   //let perms = list(itertools.permutations(range(1,8), 3))
  //bez = newbez(width, width/perms[0][2])
-bez = newbez(width, 50);
+bez = newbez(windowHeight, 50);
 let stack = []
 
 let incr = 50;
@@ -18,11 +18,14 @@ let dd = 8;
 let curldiv = 4;
 let filename = "incr"+incr+"diam"+dd+"curl"+curldiv;
 let bez_count = 0;
-while (bez_count < 40) {
-    nbez = nextbez(bez, width, b);
+let last_x = 0; 
+while (last_x < windowWidth) {
+    nbez = nextbez(bez, windowHeight, b);
     for (let l = 0; l < incr; l++) {
-        circles.push(new bezprops(bez[0], bez[1], bez[2], bez[3], incr, width, height, l, dd, 0.25));
+        circles.push(new bezprops(bez[0], bez[1], bez[2], bez[3], incr, windowWidth, windowHeight, l, dd, 0.25));
     }
+    last_x = circles[circles.length-1].y;
+
     b = b+width/curldiv;
     bez = nbez;
     bez_count++;
@@ -36,7 +39,8 @@ function draw(){
   //var strk_clr = lerpColor(color('#a3e3fe0'), color('#032edc'), frameCount/circles.length);
   var clr = '#ffffff';
   var strk_clr = '#000000';
-  diam = map(abs(circles.length/2-frameCount), 0, circles.length/2, 100, 0);
+  diam = bprops.diam;
+  //diam = map(abs(circles.length/2-frameCount), 0, circles.length/2, 100, 0);
   var ellipse = cvs.ellipse(diam, diam).attr({
     cx: bprops.y, cy: bprops.x+100, opacity: 0.5, fill: clr}).stroke({
       width: 0.05 * diam, color: strk_clr});
